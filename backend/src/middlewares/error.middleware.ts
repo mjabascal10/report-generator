be2bb20475120424fc877889dd1af03061addb80
Manuel Abascal <mjabascal10@gmail.com>
2026-02-25 20:08:30 -0600

    Add error handling middleware and logger configuration

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import {logger} from "../config/logger";
+
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+    logger.error(
+        {
+            message: err.message,
+            stack: err.stack,
+            method: req.method,
+            path: req.path,
+            body: req.body,
+            params: req.params,
+            query: req.query,
+        },
+        'Unhandled exception'
+    );
+
+    res.status(err.status || 500).json({
+        error: err.message || 'Internal server error'
+    });
+}
